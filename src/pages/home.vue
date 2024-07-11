72f95cbb3cb497da2c809d1e63b6ac2b76023004
liang <lwy.work@outlook.com>
2024-07-11 21:29:28 +0800

    feat: 完成首页

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -23,6 +23,7 @@ onLoad(() => {
 // nav导航
 // =============================
 
+// 数据渲染
 const navList = ref([])
 const getNavList = async () => {
   const {data: { message, meta }} = await uni.$http.get('/home/catitems')
@@ -33,6 +34,41 @@ const getNavList = async () => {
 }
 onLoad(()=> {
   getNavList()
+})
+
+// 点击跳转
+const navClickHandle = (item) => {
+  switch(item.name) {
+    case '分类':
+      uni.switchTab({ url: '/pages/category'})
+      console.log(item.name);
+      break
+    default:
+      break
+  }
+}
+
+// =============================
+// 楼层区
+// =============================
+
+const floorList = ref([])
+const getFloorList = async () => {
+  const { data: { message, meta }} = await uni.$http.get('/home/floordata')
+
+  if (meta.status !== 200) return uni.$showMsg()
+
+  floorList.value = message
+
+  message.forEach(floor => {
+    floor.product_list.forEach(item => {
+      item.url = '/subpackage/goods_list?' + item.navigator_url.split('?')[1]
+    })
+  })
+  console.log(message);
+}
+onLoad(()=> {
+  getFloorList()
 })
 
 // export default {
@@ -79,8 +115,44 @@ onLoad(()=> {
 
   <!-- 导航栏 -->
   <div class="nav-list">
-    <div class="nav-list-item" v-for="(item, index) in navList" :key="index">
+    <div class="nav-list-item" v-for="(item, index) in navList" :key="index" @click="navClickHandle(item)">
       <image :src="item.image_src" />
+    </div>
+  </div>
+
+  <!-- 楼层区 -->
+  <div class="floor-list">
+    <div class="floor-list-item" v-for="(item, index) in floorList" :key="index">
+      <!-- 标题 -->
+      <image
+        class="floor-title"
+        :src="item.floor_title.image_src"
+      />
+
+      <!-- 图片 -->
+      <div class="floor-image-list">
+        <!-- 大图 -->
+        <navigator :url="item.product_list[0].url">
+          <image
+            :style="{ width: item.product_list[0].image_width + 'rpx' }"
+            class="floor-image-large"
+            :src="item.product_list[0].image_src"
+            mode="widthFix"
+          />
+        </navigator>
+          
+        <!-- 小图 -->
+        <div class="floor-image-small-list">
+          <navigator v-for="(image, img_index) in item.product_list.slice(1)" :key="img_index" :url="image.url">
+            <image
+              :style="{ width: image.image_width + 'rpx' }"
+              class="floor-image-small"
+              :src="image.image_src"
+              mode="widthFix"
+            />
+          </navigator>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -106,6 +178,25 @@ swiper {
   image {
     width: 128rpx;
     height: 140rpx;
+  }
+}
+
+// 楼层区
+.floor-title {
+  width: 100%;
+  height: 60rpx;
+  display: flex;
+}
+
+.floor-image-list {
+  display: flex;
+  padding: 0 10px;
+  // flex-wrap: wrap;
+
+  .floor-image-small-list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
   }
 }
 </style>
