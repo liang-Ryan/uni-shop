0752bdea48d1b07c87fead500eb54caa832da2f8
liang-Ryan <sky_tenshi@outlook.com>
2024-07-19 21:30:36 +0800

    feat: 完成商品列表页面

---
+++ src/components/my-good.vue
@@ -0,0 +1,67 @@
+<script setup>
+// =============================
+// props
+// =============================
+
+defineProps({
+  good: Object
+})
+
+// =============================
+// 页面跳转
+// =============================
+
+const toDetail = (id) => {
+  uni.navigateTo({
+    url: `/subpackage/goods_detail?goods_id=${id}`
+  })
+}
+
+// =============================
+</script>
+
+<template>
+  <view class="goods-item" @click="toDetail(good.goods_id)">
+    <!-- 左侧-商品图片 -->
+    <view>
+      <image class="goods-item-image" :src="good.goods_small_logo" />
+    </view>
+
+    <!-- 右侧-商品详情 -->
+    <view class="goods-item-info">
+      <view class="goods-item-name">{{ good.goods_name }}</view>
+      <view class="goods-item-price">￥{{ good.goods_price.toFixed(2) }}</view>
+    </view>
+  </view>
+</template>
+
+<style lang="scss">
+.goods-item {
+  padding: 10px 5px;
+  border-bottom: 1px solid #f0f0f0;
+
+  display: flex;
+  gap: 5px;
+
+  .goods-item-image {
+    display: block;
+    width: 100px;
+    height: 100px;
+  }
+
+  .goods-item-info {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    .goods-item-name {
+      font-size: 13px;
+    }
+
+    .goods-item-price {
+      font-size: 16px;
+      color: #c00000;
+    }
+  }
+}
+</style>
--- src/subpackage/goods_list.vue
+++ src/subpackage/goods_list.vue
@@ -1,37 +1,87 @@
-<template>
-  <div class="goods_list">goods_list + 1</div>
-</template>
+<script setup>
+import { onLoad, onPullDownRefresh, onReachBottom } from '@dcloudio/uni-app'
+import { ref } from 'vue'
 
-<script>
-// import Vue from 'vue';
-// export default Vue.extend({
-//   components: {},
-//   data() {
-//     return {}
-//   },
-//   computed: {},
-//   methods: {},
-//   watch: {},
+// 组件
+import myGood from '../components/my-good.vue'
 
-//   // 页面周期函数--监听页面加载
-//   onLoad() {},
-//   // 页面周期函数--监听页面初次渲染完成
-//   onReady() {},
-//   // 页面周期函数--监听页面显示(not-nvue)
-//   onShow() {},
-//   // 页面周期函数--监听页面隐藏
-//   onHide() {},
-//   // 页面周期函数--监听页面卸载
-//   onUnload() {},
-//   // 页面处理函数--监听用户下拉动作
-//   // onPullDownRefresh() { uni.stopPullDownRefresh(); },
-//   // 页面处理函数--监听用户上拉触底
-//   // onReachBottom() {},
-//   // 页面处理函数--监听页面滚动(not-nvue)
-//   // onPageScroll(event) {},
-//   // 页面处理函数--用户点击右上角分享
-//   // onShareAppMessage(options) {},
-// }) 
+// =============================
+// 商品列表
+// =============================
+
+// 请求参数
+const queryObj = {
+  query: '',
+  cid: 5,
+  pagenum: 1,
+  pagesize: 10
+}
+
+const goods = ref({
+  goodsList: [],
+  total: 0
+})
+const getGoods = async (fn) => {
+  // 节流
+  isLoading = true
+
+  const {
+    data: { message, meta }
+  } = await uni.$http.get('/goods/search', queryObj)
+
+  if (meta.status !== 200) return uni.$showMsg()
+
+  goods.value.goodsList = [...goods.value.goodsList, ...message.goods]
+  goods.value.total = message.total
+
+  isLoading = false
+
+  // 取消下拉动画
+  fn && fn()
+}
+
+onLoad((options) => {
+  queryObj.query = options.query || ''
+  queryObj.cid = options.cid || ''
+  getGoods()
+})
+
+// =============================
+// 下拉刷新
+// =============================
+
+onPullDownRefresh(() => {
+  queryObj.pagenum = 1
+  goods.value.goodsList = []
+  goods.value.total = 0
+  isLoading = false
+
+  getGoods(() => uni.stopPullDownRefresh)
+})
+
+// =============================
+// 加载更多
+// =============================
+
+let isLoading = false
+
+onReachBottom(() => {
+  // 节流
+  if (isLoading) return
+
+  if (goods.value.goodsList.length < goods.value.total) {
+    queryObj.pagenum++
+    getGoods()
+  }
+})
+
+// =============================
 </script>
 
-<style scoped></style>
+<template>
+  <view>
+    <my-good v-for="(good, index) in goods.goodsList" :key="index" :good="good"></my-good>
+  </view>
+</template>
+
+<style></style>
